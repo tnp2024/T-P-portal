5fb8810d395f408997e990ae586d5c7e64723746
TNP <tnpportal2024@gmail.com>
2024-06-03 12:42:23 +0530

    Brief description of changes

--- finalproj2/settings.py
+++ finalproj2/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-ro9_n&0)6dh*n6nm)#cxifdv2x)w5f5pwv1*im0o(shyiz!j5%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['t-p-portal.onrender.com']
 
 
 # Application definition
